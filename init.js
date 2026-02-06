f182cd24c612e0ec417093e6ad527de6cfb91691
Vansh Mehta <vanshmehta1825@gmail.com>
2026-02-06 15:37:50 +0530

    fix mongo connection init

--- init.js
+++ init.js
@@ -10,7 +10,8 @@ main()
     })
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/Wanderlust');
+    const dbUrl = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/Wanderlust';
+    await mongoose.connect(dbUrl);
 }
 
 const sampleListings = [
